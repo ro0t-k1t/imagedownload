11dc93e29327beffbfab5e20245ba173bd9375ad
ronanphiggins <ronanhiggins8@gmail.com>
2016-03-30 08:17:32 +0100

    first commit

---
+++ app/src/main/java/com/example/ronanpiercehiggins/imagedownload/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.ronanpiercehiggins.imagedownload;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        new DownloadImageTask((ImageView) findViewById(R.id.profileimage))
+                .execute("https://api.backendless.com/A0819152-C875-C222-FF18-0516AB9ACC00/v1/files/media/me.jpg");
+
+
+
+
+
+
+
+    }
+
+
+
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+}
